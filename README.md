f12d0d0baf4d209cbb5164b3a6430f97e5c9ecbb
Beyza Köroğlu <83373912+BeyzaaKoroglu@users.noreply.github.com>
2022-12-27 00:12:45 +0300

    course model added

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 
 - Ejs
 - Express
+- Mongoose
 
 ### DevDependencies
 
--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const pageRoute = require('./routes/pageRoute');
+const courseRoute = require('./routes/courseRoute');
 
 const app = express();
+
+mongoose
+  .connect('mongodb://localhost/smartedu-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB Connected Successfully');
+  });
 
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/', pageRoute);
+app.use('/courses', courseRoute);
 
 const port = 3000;
 
