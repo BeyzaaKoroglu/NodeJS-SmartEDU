021a8fa9636c0a3e844715f6c644dc25416f8833
Beyza Köroğlu <83373912+BeyzaaKoroglu@users.noreply.github.com>
2023-01-01 18:40:58 +0300

    course-student relation

--- controllers/authController.js
+++ controllers/authController.js
@@ -22,10 +22,8 @@ exports.loginUser = async (req, res) => {
     const user = await User.findOne({ email });
     if (user) {
       bcrypt.compare(password, user.password, (err, same) => {
-        if (same) {
-          req.session.userID = user._id;
-          res.status(200).redirect('/users/dashboard');
-        }
+        req.session.userID = user._id;
+        res.status(200).redirect('/users/dashboard');
       });
     }
   } catch (error) {
@@ -43,7 +41,9 @@ exports.logoutUser = async (req, res) => {
 };
 
 exports.getDashboardPage = async (req, res) => {
-  const user = await User.findOne({ _id: req.session.userID });
+  const user = await User.findOne({ _id: req.session.userID }).populate(
+    'courses'
+  );
   const categories = await Category.find();
   const courses = await Course.find({ user: req.session.userID });
 
